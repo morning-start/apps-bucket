6392ed936cf84f41de707ec4d43453abbf229531
morning-start <morning-start@foxmail.com>
2025-07-14 16:19:25 +0800

    refactor(scripts): update config.ps1 script
    - Remove unnecessary comments
    - Replace Chinese message with English for better localization
    - Streamline code by removing redundant lines and improving readability

--- scripts/olib/config.ps1
+++ scripts/olib/config.ps1
@@ -1,7 +1,5 @@
-# 定义 JSON 文件路径
 $configFilePath = "$persist\config\config.json"
 
-# 如果文件不存在，则创建一个具有初始配置的文件
 if (-not (Test-Path -Path $configFilePath)) {
     $config = @{
         "Settings" = @{
@@ -11,14 +9,11 @@ if (-not (Test-Path -Path $configFilePath)) {
     }
 }
 else {
-    # 读取 JSON 文件内容
     $config = Get-Content -Path $configFilePath | ConvertFrom-Json
 
-    # 更新 CacheFolder 和 DownloadFolder 的值
     $config.Settings.CacheFolder = "$persist/cache"
     $config.Settings.DownloadFolder = "$persist/book"
 }
-# 将更新后的内容写回 JSON 文件
 ConvertTo-Json $config | Set-Content -Path $configFilePath
 
-Write-Host "配置已更新或初始化。"
+Write-Host "Configuration updated successfully."
