c35996952f8b8867c021fb39444986163cb3442f
morning-start <morning-start@foxmail.com>
2025-07-14 16:20:59 +0800

    fix(olib): update variable reference from $persist to $persist_dir
    - Change $persist to $persist_dir in Olib.json and config.ps1
    - Update file paths in config.ps1 to use $persist_dir

--- scripts/olib/config.ps1
+++ scripts/olib/config.ps1
@@ -1,18 +1,18 @@
-$configFilePath = "$persist\config\config.json"
+$configFilePath = "$persist_dir\config\config.json"
 
 if (-not (Test-Path -Path $configFilePath)) {
     $config = @{
         "Settings" = @{
-            "CacheFolder" = "$persist/cache"
-            "DownloadFolder" = "$persist/book"
+            "CacheFolder" = "$persist_dir/cache"
+            "DownloadFolder" = "$persist_dir/book"
         }
     }
 }
 else {
     $config = Get-Content -Path $configFilePath | ConvertFrom-Json
 
-    $config.Settings.CacheFolder = "$persist/cache"
-    $config.Settings.DownloadFolder = "$persist/book"
+    $config.Settings.CacheFolder = "$persist_dir/cache"
+    $config.Settings.DownloadFolder = "$persist_dir/book"
 }
 ConvertTo-Json $config | Set-Content -Path $configFilePath
 
