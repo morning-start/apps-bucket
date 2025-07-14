cdc6cbc40c86297e16381f8214a0b177ce5c9ccc
morning-start <morning-start@foxmail.com>
2025-07-14 15:30:13 +0800

    feat(Olib): add post-install script and update persistence settings
    - Add post_install field to Olib.json pointing to Olb.ps1 script
    - Include 'cache' and 'book' directories in persist settings
    - Create Olib.ps1 script to initialize and update configuration

---
+++ scripts/Olib.ps1
@@ -0,0 +1,23 @@
+# 定义 JSON 文件路径
+$configFilePath = "$persist\config\config.json"
+
+# 如果文件不存在，则创建一个具有初始配置的文件
+if (-not (Test-Path -Path $configFilePath)) {
+    $initialConfig = @{
+        CacheFolder    = ""
+        DownloadFolder = ""
+    }
+    ConvertTo-Json $initialConfig | Set-Content -Path $configFilePath
+}
+
+# 读取 JSON 文件内容
+$config = Get-Content -Path $configFilePath | ConvertFrom-Json
+
+# 更新 CacheFolder 和 DownloadFolder 的值
+$config.CacheFolder = "$persist/cache"
+$config.DownloadFolder = "$persist/book"
+
+# 将更新后的内容写回 JSON 文件
+ConvertTo-Json $config | Set-Content -Path $configFilePath
+
+Write-Host "配置已更新或初始化。"
