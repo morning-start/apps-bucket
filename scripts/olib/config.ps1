71b174b2d0f038323452b280c17444b10a471683
morning-start <morning-start@foxmail.com>
2025-07-14 16:29:29 +0800

    fix(bucket): update Olib.json and config.ps1 for Windows compatibility
    - Update Olib.json to use Invoke-Expression instead of executing config.ps1 directly
    - Modify config.ps1 to use backslashes for Windows path separators

--- scripts/olib/config.ps1
+++ scripts/olib/config.ps1
@@ -3,16 +3,16 @@ $configFilePath = "$persist_dir\config\config.json"
 if (-not (Test-Path -Path $configFilePath)) {
     $config = @{
         "Settings" = @{
-            "CacheFolder" = "$persist_dir/cache"
-            "DownloadFolder" = "$persist_dir/book"
+            "CacheFolder" = "$persist_dir\cache"
+            "DownloadFolder" = "$persist_dir\book"
         }
     }
 }
 else {
     $config = Get-Content -Path $configFilePath | ConvertFrom-Json
 
-    $config.Settings.CacheFolder = "$persist_dir/cache"
-    $config.Settings.DownloadFolder = "$persist_dir/book"
+    $config.Settings.CacheFolder = "$persist_dir\cache"
+    $config.Settings.DownloadFolder = "$persist_dir\book"
 }
 ConvertTo-Json $config | Set-Content -Path $configFilePath
 
