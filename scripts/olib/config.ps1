8ae0f41ce2a0f1e16eba6a2aed9855edea61741b
morning-start <morning-start@foxmail.com>
2025-07-14 15:57:44 +0800

    refactor(config): simplify initial configuration and update config.ps1
    - Remove unnecessary default configuration from config.json
    - Update config.ps1 to create initial configuration only if needed
    - Adjust configuration update process in config.ps1

--- scripts/olib/config.ps1
+++ scripts/olib/config.ps1
@@ -3,16 +3,21 @@ $configFilePath = "$persist\config\config.json"
 
 # 如果文件不存在，则创建一个具有初始配置的文件
 if (-not (Test-Path -Path $configFilePath)) {
-    Copy-Item -Path "./config.json" -Destination $configFilePath
+    $config = @{
+        "Settings" = @{
+            "CacheFolder" = "$persist/cache"
+            "DownloadFolder" = "$persist/book"
+        }
+    }
 }
+else {
+    # 读取 JSON 文件内容
+    $config = Get-Content -Path $configFilePath | ConvertFrom-Json
 
-# 读取 JSON 文件内容
-$config = Get-Content -Path $configFilePath | ConvertFrom-Json
-
-# 更新 CacheFolder 和 DownloadFolder 的值
-$config.Settings.CacheFolder = "$persist/cache"
-$config.Settings.DownloadFolder = "$persist/book"
-
+    # 更新 CacheFolder 和 DownloadFolder 的值
+    $config.Settings.CacheFolder = "$persist/cache"
+    $config.Settings.DownloadFolder = "$persist/book"
+}
 # 将更新后的内容写回 JSON 文件
 ConvertTo-Json $config | Set-Content -Path $configFilePath
 
